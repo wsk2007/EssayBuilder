cfc97cfb4ab49ab8db1c5fe61569d4b217a8c59a
BuildTools <unconfigured@null.spigotmc.org>
2020-01-30 22:32:22 +0800

    2020.1.30 22:32:00 实现builder并可以使用

--- EssayBuilder/src/org/wsk/EssayBuilder/Builder.java
+++ EssayBuilder/src/org/wsk/EssayBuilder/Builder.java
@@ -1,5 +1,51 @@
 package org.wsk.EssayBuilder;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
 public class Builder {
+	SecureRandom sr = new SecureRandom();
+	ArrayList<String> famous;
+	ArrayList<String> bosh;
+	ArrayList<String> before;
+	ArrayList<String> after;
+	public Builder(ArrayList<String> famous,ArrayList<String> bosh,ArrayList<String> before,ArrayList<String> after) {
+		this.famous = famous;
+		this.bosh = bosh;
+		this.before = before;
+		this.after = after;
+		
+	}
+	
+	public String Build(String theme, int length) {
+		StringBuilder sb = new StringBuilder();
+		int num;
+		sb.append("    ");
+		while(sb.length() < length) {
+			num = sr.nextInt(100);
+			if(num < 5) {
+				sb.append("\r\n");
+				sb.append("    ");
+			}if(num < 30) {
+				sb.append(randomChoice(famous).replace("a", randomChoice(before)).replace("b", randomChoice(after)));
+						
+				num = sr.nextInt(100);
+				if(num < 50) {
+					sb.append("\r\n");
+					sb.append("    ");
+				}
+			
+				
+			}if(num < 100) {
+				sb.append(randomChoice(bosh).replaceAll("x", theme));
+				
+			}
+		}
+		return sb.toString();
+	}
+	
+	public <T> T randomChoice(ArrayList<T> data) {
+		return data.get(sr.nextInt(data.size()));
+	}
 
 }
--- EssayBuilder/src/org/wsk/EssayBuilder/Main.java
+++ EssayBuilder/src/org/wsk/EssayBuilder/Main.java
@@ -6,27 +6,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		Download d = new Download();
-		try {
+		
+		
+			try {
 			d.saveToFile("https://raw.githubusercontent.com/wsk2007/NonsenseForEssayBuilder/master/data.json", "data.json");
+			
 		}
 			
 			// TODO 自动生成的 catch 块
 			catch(Exception e) {
 			e.printStackTrace();
+			
 		}
+		
+		
 		Json j = new Json();
 		String[] s = {"famous" , "bosh" , "after" , "before"};
 		ArrayList<ArrayList<String>> al = j.getdata("data.json", s);
+		Builder b = new Builder(al.get(0),al.get(1),al.get(3),al.get(2));
+		System.out.println(b.Build("好好学习", 5000));
 		
-		for(ArrayList<String> a : al) {
-			System.out.println("-------------------------");
-			for(String str : a) {
-				System.out.println(str);
-			}
 		}
 
 
 	
 
-}
 }
