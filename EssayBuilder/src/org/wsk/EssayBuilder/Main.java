d6c9705dcf5144b6d876ea7abea8ac5283326451
BuildTools <unconfigured@null.spigotmc.org>
2020-01-30 13:45:30 +0800

    2020.1.30 13:50:00 文章生成器实现文件下载功能

--- EssayBuilder/src/org/wsk/EssayBuilder/Main.java
+++ EssayBuilder/src/org/wsk/EssayBuilder/Main.java
@@ -1,10 +1,24 @@
 package org.wsk.EssayBuilder;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Download d = new Download();
+		try {
+			d.saveToFile("https://raw.githubusercontent.com/wsk2007/NonsenseForEssayBuilder/master/data.json", "C:\\AppData\\Roaming\\EssayBuilder\\data.json");
+		}
+			
+			// TODO 自动生成的 catch 块
+			catch(Exception e) {
+			e.printStackTrace();
+		}
 
-	}
 
+	
+
+}
 }
